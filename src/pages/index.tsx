eb86d091d33c38c6d4c1342f3622db215db40408
daiti0113 <stennis.ds.0113@gmail.com>
2022-05-26 23:05:20 +0900

    feat: メーカー、車種によって料金を表示する仕組みを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,69 @@
 import type { NextPage } from 'next'
-import { Button, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField, Typography } from '@mui/material'
-import { useState } from 'react'
-
-const makers = [
-  { label: "トヨタ", value: "toyota" },
-  { label: "日産", value: "nissan" },
-  { label: "ホンダ", value: "honda" },
-  { label: "マツダ", value: "mazda" },
-  { label: "三菱", value: "mitsubishi" },
-  { label: "スバル", value: "subaru" },
-  { label: "ダイハツ", value: "daihatsu" },
-  { label: "スズキ", value: "suzuki" },
-]
+import { Button, Divider, FormControl, FormControlLabel, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField, Typography } from '@mui/material'
+import { useMemo, useState } from 'react'
+import { getCars, getPrice, makers } from '../db'
 
 const Home: NextPage = () => {
+  const [peelOff, setPeelOff] = useState("0")
   const [maker, setMaker] = useState("")
+  const [car, setCar] = useState<{name: string|null, frontSet: number|null, rearSet: number|null} | undefined>({name: null, frontSet: null, rearSet: null})
+  const [showResult, setShowResult] = useState(false)
+  const cars = useMemo(() => getCars(maker), [maker])
+
   return (
-    <FormControl>
-      <Typography variant="h2" component="h1" gutterBottom>
-        カーフィルム施工見積もり
-      </Typography>
-      <FormLabel id="demo-row-radio-buttons-group-label">フィルムの剥がし</FormLabel>
-      <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
-        <FormControlLabel value="peelOff" control={<Radio />} label="あり" />
-        <FormControlLabel value="notPeelOff" control={<Radio />} label="なし" />
-      </RadioGroup>
-      <FormLabel id="demo-row-radio-buttons-group-label">メーカー</FormLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={maker}
-        label="メーカー"
-        onChange={(event) => setMaker(event.target.value)}
-      >
-        {makers.map(({ label, value }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
-      </Select>
-      <FormLabel id="demo-row-radio-buttons-group-label">車種</FormLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={maker}
-        label="maker"
-        onChange={(event) => setMaker(event.target.value)}
-      >
-        {makers.map(({ label, value }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
-      </Select>
-      <Button variant="contained">検索</Button>
-    </FormControl>
+    <div>
+      <FormControl>
+        <Typography variant="h2" component="h1" gutterBottom>
+          カーフィルム施工見積もり
+        </Typography>
+        <FormControl>
+          <FormLabel id="demo-row-radio-buttons-group-label">フィルムの剥がし</FormLabel>
+          <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={peelOff} onChange={event => setPeelOff(event.target.value)}>
+            <FormControlLabel value="0" control={<Radio />} label="あり" />
+            <FormControlLabel value="1" control={<Radio />} label="なし" />
+          </RadioGroup>
+          <FormHelperText>古いフィルムの剥がし作業がある場合は、別途料金がかかります</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="demo-row-radio-buttons-group-label">メーカー</FormLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={maker}
+            label="メーカー"
+            onChange={(event) => setMaker(event.target.value)}
+          >
+            {makers.map(({ label, value }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="demo-row-radio-buttons-group-label">車種</FormLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={car?.name}
+            label="maker"
+            onChange={(event) => {
+              const car = getPrice(maker, event.target.value)
+              setCar(car)
+            }}
+            disabled={!cars}
+          >
+            {cars && cars.map(({ name }) => <MenuItem key={name} value={name}>{name}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <Button variant="contained" onClick={() => setShowResult(true)} disabled={!car}>お見積り</Button>
+        </FormControl>
+        <Divider />
+        {showResult && (
+          <div>
+            <Typography variant="h3" component="h2" gutterBottom>お見積り内容</Typography>
+            {<Typography variant="body1" component="p" gutterBottom>フロント3面: {car?.frontSet}</Typography>}
+            {<Typography variant="body1" component="p" gutterBottom>リア3面: {car?.rearSet}</Typography>}
+            {peelOff && <Typography variant="body1" component="p" gutterBottom>お手数をおかけしますが、剥がし料金はお問い合わせください</Typography>}
+          </div>
+        )}
+    </div>
   )
 }
 
