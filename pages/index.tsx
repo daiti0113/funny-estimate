028c2a0a1320250e6adeb109cb564d5e6ccc01c1
daiti0113 <stennis.ds.0113@gmail.com>
2022-05-27 13:16:01 +0900

    feat: フィルム剥がしのフォームを削除 && 結果の表示方法を変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
-import { Button, Divider, FormControl, FormControlLabel, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField, Typography, useTheme } from '@mui/material'
+import { Button, Divider, FormControl, FormControlLabel, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography, useTheme } from '@mui/material'
 import { useMemo, useState } from 'react'
 import { getCars, getPrice, makers } from '../db'
 import { css } from "@emotion/react"
 
 const Home: NextPage = () => {
-  const [peelOff, setPeelOff] = useState("0")
   const [maker, setMaker] = useState("")
   const [car, setCar] = useState<{name: string|null, frontSet: number|null, rearSet: number|null} | undefined>({name: null, frontSet: null, rearSet: null})
   const [showResult, setShowResult] = useState(false)
@@ -19,14 +18,6 @@ const Home: NextPage = () => {
         カーフィルム施工見積もり
       </Typography>
       <FormControl css={styles.formContainer}>
-        <FormControl>
-          <FormLabel id="demo-row-radio-buttons-group-label">フィルムの剥がし</FormLabel>
-          <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={peelOff} onChange={event => setPeelOff(event.target.value)}>
-            <FormControlLabel value="0" control={<Radio />} label="あり" />
-            <FormControlLabel value="1" control={<Radio />} label="なし" />
-          </RadioGroup>
-          <FormHelperText>古いフィルムの剥がし作業がある場合は、別途料金がかかります</FormHelperText>
-        </FormControl>
         <FormControl>
           <FormLabel id="demo-row-radio-buttons-group-label">メーカー</FormLabel>
           <Select
@@ -57,13 +48,17 @@ const Home: NextPage = () => {
         </FormControl>
         <Button variant="contained" onClick={() => setShowResult(true)} disabled={!car}>お見積り</Button>
         </FormControl>
-        <Divider />
+        <Divider css={css({margin: 30})}/>
         {showResult && (
-          <div>
-            <Typography variant="h3" component="h2" gutterBottom>お見積り内容</Typography>
-            {<Typography variant="body1" component="p" gutterBottom>フロント3面: {car?.frontSet}</Typography>}
-            {<Typography variant="body1" component="p" gutterBottom>リア3面: {car?.rearSet}</Typography>}
-            {peelOff && <Typography variant="body1" component="p" gutterBottom>お手数をおかけしますが、剥がし料金はお問い合わせください</Typography>}
+          <div css={styles.resultContainer}>
+            <Typography variant="h4" component="h2" gutterBottom>お見積り内容</Typography>
+            {<Typography variant="h5" component="p" gutterBottom>{car?.name}</Typography>}
+            {car && <PriceTable car={car} />}
+            <div css={css({marginTop: 30})}>
+              <Typography variant="body1" component="p" gutterBottom>※古いフィルムの剥がし作業がある場合は、別途料金がかかります。</Typography>
+              <Typography variant="body1" component="p" gutterBottom>※料金は年式やグレード等により若干前後する場合がございます。</Typography>
+              <Typography variant="body1" component="p" gutterBottom>詳細については、お電話にてお問い合わせください。</Typography>
+            </div>
           </div>
         )}
     </div>
@@ -83,5 +78,41 @@ const styles = {
     maxWidth: "400px",
     padding: "0 40px",
     margin: "40px auto 0 auto"
+  }),
+  resultContainer: css({
+    rowGap: "30px",
+    maxWidth: "600px",
+    padding: "0 40px",
+    margin: "40px auto 0 auto"
   })
+}
+
+
+const PriceTable = ({car}: {car: {name: string|null, frontSet: number|null, rearSet: number|null}}) => {
+  return (
+    <TableContainer>
+      <Table sx={{maxWidth: 400}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>内容</TableCell>
+            <TableCell align="right">料金</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow key="frontSet">
+              <TableCell component="th" scope="row">
+                フロント3面
+              </TableCell>
+              <TableCell align="right">{car.frontSet}</TableCell>
+            </TableRow>
+            <TableRow key="frontSet">
+              <TableCell component="th" scope="row">
+                リア3面
+              </TableCell>
+              <TableCell align="right">{car.rearSet}</TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
